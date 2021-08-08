828041b75a37626ad5882e2c5e962316c6dd090b
Yuriekokubu <60240233118@udru.ac.th>
2021-08-08 09:52:06 +0700

    sdfsdf

--- src/__tests__/components/card.test.js
+++ src/__tests__/components/card.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Card, Player } from '../../components';
 
-const category = 'series';
+const category = 'series';dd
 const slideRows = [
   {
     title: 'Documentaries',
