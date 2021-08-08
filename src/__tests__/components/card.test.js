95157f2c2f5d691e9f1f79215e5519791e646ca0
Yuriekokubu <60240233118@udru.ac.th>
2021-08-08 09:50:41 +0700

    test reset head

--- src/__tests__/components/card.test.js
+++ src/__tests__/components/card.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Card, Player } from '../../components';
 
-const category = 'series';
+const category = 'series'; sdfsdf
 const slideRows = [
   {
     title: 'Documentaries',
